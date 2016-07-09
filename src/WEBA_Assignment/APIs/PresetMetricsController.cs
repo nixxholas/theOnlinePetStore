8c4fda6ffcdaa49c485d2b0c441f04b4f0e1ad8d
nixxholas <amperific@gmail.com>
2016-07-10 01:02:08 +0800

    Products/Views: Create.cshtml: 60% Complete

--- src/WEBA_Assignment/APIs/PresetMetricsController.cs
+++ src/WEBA_Assignment/APIs/PresetMetricsController.cs
@@ -1,28 +1,68 @@
-﻿using System;
+﻿using WEBA_ASSIGNMENT.Models;
+//Need InternshipManagementSystemWithSecurity_V1.Data so that the .NET can find
+//the ApplicationDbContext class.
+using WEBA_ASSIGNMENT.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using WEBA_ASSIGNMENT.Services;
+using Microsoft.Extensions.Logging;
+using WEBA_ASSIGNMENT.Controllers;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WEBA_ASSIGNMENT.APIs
 {
+    [Authorize("RequireAdminRole")]
     [Route("api/[controller]")]
     public class PresetMetricsController : Controller
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IEmailSender _emailSender;
+        private readonly ISmsSender _smsSender;
+        private readonly ILogger _logger;
+
+        //Every Web API controller class need to have an ApplicationDbContext
+        //type property. I have declared one here, called Database.
+        public ApplicationDbContext Database { get; }
+        public PresetMetricsController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IEmailSender emailSender,
+            ISmsSender smsSender,
+            ILoggerFactory loggerFactory)
         {
-            return new string[] { "value1", "value2" };
+            Database = new ApplicationDbContext();
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
+            _smsSender = smsSender;
+            _logger = loggerFactory.CreateLogger<AccountController>();
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET: api/values
+        [HttpGet]
+        public JsonResult Get()
         {
-            return "value";
+            List<object> presetMetricsList = new List<object>();
+            var presetmetrics = Database.PresetMetrics;
+
+            foreach (var presetmetric in presetmetrics)
+            {
+                presetMetricsList.Add(new
+                {
+                    PMetricId = presetmetric.PMetricId,
+                    MetricType = presetmetric.MetricType,
+                    MetricSubType = presetmetric.MetricSubType,
+                    MetricCharacter = presetmetric.MetricCharacter
+                });
+            }//end of foreach loop which builds the categoryList .
+            return new JsonResult(presetMetricsList);
         }
 
         // POST api/values
