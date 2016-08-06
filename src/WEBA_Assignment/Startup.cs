7d2a41f453c58aec05666ece57ce269919472010
Nicholas <amperific@gmail.com>
2016-08-06 12:19:15 +0800

    Views: Products: Update/Create: Publish Status Fixed
    - Various updates to bower packages as well

--- src/WEBA_Assignment/Startup.cs
+++ src/WEBA_Assignment/Startup.cs
@@ -100,7 +100,7 @@ namespace WEBA_ASSIGNMENT
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
-          //app.SeedData();
+          app.SeedData();
         }//end of Configure()
     }
 }
